205fc1bd61abbfb77ff5266d09fe9755df88f17b
iamcody-git <iamcody.github@gmail.com>
2025-02-23 13:10:09 +0545

    first commit

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+import { Link, NavLink } from "react-router-dom";
+const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+     <img src={assets.logo2} alt="" className="w-24 h-20" />
+      <ul className="hidden sm:flex gap-5 text-sm text-grey-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>Home</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+          <p>Collection</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>About</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>Contact</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
+
+      <div className="flex items-center gap-6">
+        <img src={assets.search_icon} alt="" className="w-5 cursor-pointer" />
+        <div className="group relative">
+          <img
+            className="w-5 cursor-pointer"
+            src={assets.profile_icon}
+            alt="Profile"
+          />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded shadow-lg">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
+          <p className="absolute right-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded">
+            10
+          </p>
+        </Link>
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+          alt=""
+        />
+      </div>
+      {/* sidebar menu for small screen */}
+      <div
+        className={`fixed top-0 right-0 h-screen w-[70%] bg-white transition-transform duration-300 sm:hidden 
+  ${visible ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-4 cursor-pointer border-b"
+          >
+            <img className="h-4 rotate-180" src={assets.dropdown_icon} alt="" />
+            <p>Back</p>
+          </div>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-4 px-6 border-b text-lg"
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-4 px-6 border-b text-lg"
+            to="/collection"
+          >
+            Collection
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-4 px-6 border-b text-lg"
+            to="/about"
+          >
+            About
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-4 px-6 border-b text-lg"
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
