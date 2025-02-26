a2d8bcdca90fcde8b1b48d32eec959dd4a6eb04e
iamcody-git <iamcody.github@gmail.com>
2025-02-26 21:48:49 +0545

    add to cart implement

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,10 +13,13 @@ import PlaceOrder from './pages/PlaceOrder'
 import Contact from './pages/Contact'
 import Cart from './pages/Cart'
 import Footer from './components/Footer'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
       <Navbar />
       <Routes >
         <Route path='/' element={<Home />} />
--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,24 +1,64 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
- export const ShopContext = createContext();
+export const ShopContext = createContext();
 
- const ShopContextProvider = (props)=>{
-    const currency = 'Rs';
-    const delivery_fee = 100;
-    const [search, setSearch] = useState('');
-    const [showSearch, setShowSearch] = useState(false);
+const ShopContextProvider = (props) => {
+  const currency = "Rs ";
+  const delivery_fee = 100;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItem, setCartItem] = useState({});
 
-    const value={
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch
-
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Select product size.");
+      return;
     }
 
-    return(
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    )
- }
+    let CartData = structuredClone(cartItem);
 
- export default ShopContextProvider
+    if (CartData[itemId]) {
+      if (CartData[itemId][size]) {
+        CartData[itemId][size] += 1;
+      } else {
+        CartData[itemId][size] = 1;
+      }
+    } else {
+      CartData[itemId] = {};
+      CartData[itemId][size] = 1;
+    }
+    setCartItem(CartData);
+  };
+
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItem) {
+        for (const size in cartItem[items]) {
+            totalCount += cartItem[items][size]; 
+        }
+    }
+    return totalCount;
+};
+
+
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    cartItem,
+    addToCart,
+    getCartCount
+  };
+
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
