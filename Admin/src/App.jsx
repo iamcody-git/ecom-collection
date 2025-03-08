9adf97a8271d1a2d46e06a02b901dd41f2e11d75
iamcody-git <iamcody.github@gmail.com>
2025-03-08 10:17:12 +0545

    admin started

---
+++ Admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+    <div className='text-4xl text-center bg-amber-200'>Heoooosh</div>
+    </>
+  )
+}
+
+export default App
