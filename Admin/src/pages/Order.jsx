3154f9c5f31de533253a05c20196fdcea6f3edda
iamcody-git <iamcody.github@gmail.com>
2025-03-16 12:02:26 +0545

    admin done

--- Admin/src/pages/Order.jsx
+++ Admin/src/pages/Order.jsx
@@ -28,6 +28,23 @@ const Order = ({ token }) => {
     }
   };
 
+  const statusHandler = async (event, orderId) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/status",
+        { orderId, status: event.target.value },
+        { headers: { token } }
+      );
+
+      if (response.data.success) {
+        await fetchAllOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message);
+    }
+  };
+
   useEffect(() => {
     fetchAllOrders();
   }, [token]);
@@ -37,7 +54,10 @@ const Order = ({ token }) => {
       <h3>Order Page</h3>
       <div>
         {orders.map((order, index) => (
-          <div className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 text-xs sm:text-sm text-gray-600" key={index}>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 text-xs sm:text-sm text-gray-600"
+            key={index}
+          >
             <img className="w-12" src={assets.parcel_icon} alt="" />
             <div>
               <div>
@@ -57,9 +77,7 @@ const Order = ({ token }) => {
                     );
                   } else {
                     return (
-                      <p 
-                      className="py-0.5"
-                      key={index}>
+                      <p className="py-0.5" key={index}>
                         {itemName} x {item.quantity} <span>{item.size},</span>
                       </p>
                     );
@@ -67,7 +85,9 @@ const Order = ({ token }) => {
                 })}
               </div>
               {/* Accessing order.address inside the map */}
-              <p className="mt-3 mb-2 font-medium">{order.address.firstName + " " + order.address.lastName}</p>
+              <p className="mt-3 mb-2 font-medium">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
               <div>
                 <p>{order.address.street + ",  "}</p>
                 <p>
@@ -82,14 +102,22 @@ const Order = ({ token }) => {
               </div>
             </div>
             <div>
-             
-              <p className="text-sm sm:text-[15px]">Item:{order.items.length}</p>
+              <p className="text-sm sm:text-[15px]">
+                Item:{order.items.length}
+              </p>
               <p className="mt-3">Method:{order.paymentMethod}</p>
-              <p>Payment:{order.payment ? "Done":"Pending"}</p>
+              <p>Payment:{order.payment ? "Done" : "Pending"}</p>
               <p>Date:{new Date(order.date).toLocaleDateString()}</p>
             </div>
-            <p className="text-sm sm:text-[15px]">{currency}{order.amount}</p>
-            <select className="p-2 font-semibold">
+            <p className="text-sm sm:text-[15px]">
+              {currency}
+              {order.amount}
+            </p>
+            <select
+              onChange={(event) => statusHandler(event, order._id)}
+              value={order.status}
+              className="p-2 font-semibold"
+            >
               <option value="OrderPlaced">Order Placed</option>
               <option value="Packing">Packing</option>
               <option value="Shipped">Shipped</option>
--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -78,4 +78,16 @@ export const userOrders = async (req, res) => {
 };
 
 // update order status from admin panel
-export const updateStatus = async (req, res) => {};
+export const updateStatus = async (req, res) => {
+  try {
+    const {orderId, status} = req.body;
+    await orderModel.findByIdAndUpdate(orderId, {status})
+
+    res.json({success:true, message:'Status updated'})
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+
+    
+  }
+};
